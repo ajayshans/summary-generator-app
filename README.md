a1d535aab0ec70f00f39852119d7a412949c0fe9
Ajay S <ajaycshan@gmail.com>
2024-02-13 16:12:12 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# GPT Summary Generation App
---
+++ script.js
@@ -0,0 +1,5 @@
+// Import dotenv to access environment variables
+require('dotenv').config();
+
+// Run below to test API Key is correct
+// console.log(process.env.OPENAI_API_KEY);
